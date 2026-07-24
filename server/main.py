7a7c5be81355e1cc67c2ee6157dd1d2ca307d68c
Oladele Magbadelo Jr <omagbad@uwo.ca>
2026-07-24 10:21:32 +0300

    fix: put .gitignore @ root, changed origins to be both local dev and cloud dev

--- server/main.py
+++ server/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import os
+
+origins = os.getenv("ALLOWED_ORIGINS", "http://localhost:3000").split(",")
 
 app = FastAPI()
 
@@ -7,7 +10,7 @@ app = FastAPI()
 # so frontend and backend dont get blocked tryna talk to each other
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins= origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
